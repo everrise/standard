ecad8dcd0380f0aa74db7f3a459a02f6511f0c94
Tai T. Nguyen <tai.nguyen@everrise.asia>
2013-07-25 11:52:45 +0700

    import

--- ersample/src/main/java/jp/co/everrisesample/minimum01/entity/Names.java
+++ ersample/src/main/java/jp/co/everrisesample/minimum01/entity/Names.java
@@ -1,36 +1,17 @@
 package jp.co.everrisesample.minimum01.entity;
 
-import javax.annotation.Generated;
-import jp.co.everrisesample.minimum01.entity.ArticleNames._ArticleNames;
-import jp.co.everrisesample.minimum01.entity.CategoryNames._CategoryNames;
 import jp.co.everrisesample.minimum01.entity.ChartNames._ChartNames;
 import jp.co.everrisesample.minimum01.entity.UserNames._UserNames;
 
 /**
  * Summary of names class
- * 
+ *
  */
 public class Names {
 
     /**
-     * return name class of {@link Article}
-     * 
-     */
-    public static _ArticleNames article() {
-        return new _ArticleNames();
-    }
-
-    /**
-     * return name class of {@link Category}
-     * 
-     */
-    public static _CategoryNames category() {
-        return new _CategoryNames();
-    }
-
-    /**
      * return name class of {@link Chart}
-     * 
+     *
      */
     public static _ChartNames chart() {
         return new _ChartNames();
@@ -38,7 +19,7 @@ public class Names {
 
     /**
      * return name class of {@link User}
-     * 
+     *
      */
     public static _UserNames user() {
         return new _UserNames();
