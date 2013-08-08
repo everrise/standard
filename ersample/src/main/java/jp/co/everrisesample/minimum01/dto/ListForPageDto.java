bbec457d0a01e4bae9467619fccac58c30ff6fff
Doan Du <du.doan@everrise.asia>
2013-08-08 10:09:05 +0700

    Update ListForPageDto.java
    Signed-off-by: Doan Du <du.doan@everrise.asia>

--- ersample/src/main/java/jp/co/everrisesample/minimum01/dto/ListForPageDto.java
+++ ersample/src/main/java/jp/co/everrisesample/minimum01/dto/ListForPageDto.java
@@ -5,42 +5,65 @@ import java.util.List;
 /**
  * 一覧表示画面へ値を持っていくための入れ物
  */
-public class ListForPageDto<T>{
-    public List<T> resultList;
-    public int page;
-    public int limit;
-    public long total;
-    public String name;
-    public String orderBy;
+public class ListForPageDto<T> {
+	public List<T> resultList;
+	public int page;
+	public int limit;
+	public long total;
+	public String name;
+	public String orderBy;
 
-    /**
-     * 最終ページを得る
-     * @return
-     */
-    public int getMaxPage(){
-        if(total == 0){
-            return 0;
-        }
-        if((total % limit) == 0){
-            return (int) (total / limit);
-        }
-        return (int) (total / limit) + 1;
-    }
+	/**
+	 * 最終ページを得る
+	 *
+	 * @return
+	 */
+	public int getMaxPage() {
+		if (total == 0) {
+			return 0;
+		}
+		if ((total % limit) == 0) {
+			return (int) (total / limit);
+		}
+		return (int) (total / limit) + 1;
+	}
 
-    public boolean getHasFirst(){
-        return true;
-    }
+	public int getFirstRow() {
+		return (page - 1) * limit + 1;
+	}
 
-    public boolean getHasPrev(){
-        return true;
-    }
+	public int getLastRow() {
+		if (total == limit * page) {
+			return limit * page;
+		}
+		if (hasNext()) {
+			return getFirstRow() + limit - 1;
+		}
+		return (int) (getFirstRow() + (total % limit)) - 1;
+	}
 
-    public boolean getHasNext(){
-        return true;
-    }
+	public boolean hasNext() {
+		return (getMaxPage() > 1 && page < getMaxPage());
+	}
 
-    public boolean getHasLast(){
-        return true;
-    }
+	public boolean getHasFirst() {
+		return true;
+	}
+
+	public boolean getHasNext() {
+		return hasNext();
+	}
+
+	public boolean hasPrev() {
+		return page > 1;
+	}
+
+	public boolean getHasPrev() {
+		return hasPrev();
+	}
+
+	public boolean getHasLast() {
+		return true;
+	}
 
 }
